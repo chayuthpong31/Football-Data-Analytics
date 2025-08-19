74e150beb2ea1b8f8143086ea94f7d8c8cc18bc1
chayuthpong31 <chayuthpong31@gmail.com>
2025-08-19 22:25:10 +0700

    create extract dag

--- dags/wikipedia_flow.py
+++ dags/wikipedia_flow.py
@@ -1,8 +1,13 @@
 from airflow import DAG
 from datetime import datetime
 
+import os
+import sys
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from airflow.operators.python import PythonOperator
-from pipelines.wikipedia_pipelines import get_wikipedia_page
+from pipelines.wikipedia_pipelines import extract_wikipedia_data
 
 dag = DAG(
     dag_id='wikipedia_flow',
@@ -10,15 +15,14 @@ dag = DAG(
         'owner':'Chayuthpong31',
         'start_date': datetime(year=2025, month=8, day=19)
     },
-    schedule_interval=None,
+    schedule=None,
     catchup=False
 )
 
 # Extraction
 extract_data_from_wikipedia = PythonOperator(
     task_id="extract_data_from_wikipedia",
-    python_callable=get_wikipedia_page,
-    provide_context=True,
+    python_callable=extract_wikipedia_data,
     op_kwargs={"url": "https://en.wikipedia.org/wiki/List_of_association_football_stadiums_by_capacity"},
     dag=dag
 )
--- pipelines/wikipedia_pipelines.py
+++ pipelines/wikipedia_pipelines.py
@@ -7,6 +7,42 @@ def get_wikipedia_page(url):
         response = requests.get(url, timeout=10)
         response.raise_for_status() # check if the request is successful
 
-        response.text
+        return response.text
     except requests.RequestException as e:
         print(f"An error occured: {e}")
+
+def get_wikipedia_data(html):
+    from bs4 import BeautifulSoup
+
+    soup = BeautifulSoup(html, features="html.parser")
+    table = soup.find_all("table", attrs={"class":"wikitable"})[1]
+
+    table_rows = table.find_all('tr')
+
+    return table_rows
+
+def extract_wikipedia_data(**kwargs):
+    import pandas as pd
+    url = kwargs["url"]
+    html = get_wikipedia_page(url)
+    rows = get_wikipedia_data(html)
+
+    data = []
+
+    for i in range(1, len(rows)):
+        tds = rows[i].find_all('td')
+        values = {
+            'rank':i,
+            'stadium':tds[0].text,
+            'capacity':tds[1].text,
+            'region':tds[2].text,
+            'country':tds[3].text,
+            'city':tds[4].text,
+            'images':tds[5].find('img').get('src').split("//")[1] if tds[5].find('img') else "NO_IMAGE" ,
+            'home_team':tds[6].text
+        }
+        data.append(values)
+    
+    data_df = pd.DataFrame(data)
+    data_df.to_csv("data/output.csv", index=False)
+    return data_df
